edcd07b407da0831fb758c3cb31c1b2d1007a7cb
Stupnikjs <n.boudier.ph@gmail.com>
2024-09-20 22:17:16 +0200

    o

--- files.zig
+++ files.zig
@@ -4,6 +4,10 @@ const print = std.debug.print;
 const fileError = error{
     extentionNotFound,
 };
+const fileStat = struct {
+    name: []const u8,
+    size: u64,
+};
 
 pub fn listFiles() !void {
     const dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
@@ -15,16 +19,49 @@ pub fn listFiles() !void {
     }
 }
 
-pub fn FilesWeigth() !void {
-    const dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
-    var walker = try dir.walk(std.heap.page_allocator);
-    defer walker.deinit();
-    while (try walker.next()) |entry| {
-        if (entry.path[0] == '.') continue;
-        const file = try std.fs.cwd().openFile(entry.basename, .{});
+pub fn FilesWeigth(dir_path: []const u8) !void {
+    var dir = try std.fs.openDirAbsolute(dir_path, .{ .iterate = true });
+    defer dir.close();
+    var iter = dir.iterate();
+
+    while (try iter.next()) |entry| {
+        if (entry.kind == std.fs.File.Kind.directory) {
+            print("{s} \n", .{entry.name});
+            continue;
+        }
+        if (entry.name[0] == '.') continue;
+        print("{s} \n", .{entry.name});
+        const file = try dir.openFile(entry.name, .{});
         const stat = try file.stat();
         print("{d} bytes \n", .{stat.size / 8});
     }
+}
+
+pub fn BiggestFile(dir_path: []const u8) !fileStat {
+    var dir = try std.fs.openDirAbsolute(dir_path, .{ .iterate = true });
+    defer dir.close();
+    var iter = dir.iterate();
+    var bigger = &fileStat{
+        .name = "",
+        .size = 0,
+    };
+    while (try iter.next()) |entry| {
+        if (entry.kind == std.fs.File.Kind.directory) {
+            print("{s} \n", .{entry.name});
+            continue;
+        }
+        if (entry.name[0] == '.') continue;
+        const file = try dir.openFile(entry.name, .{});
+        const stat = try file.stat();
+        if (bigger.size < stat.size) {
+            bigger = fileStat{
+                .name =  
+            }
+            
+        }
+        print("{d} bytes \n", .{stat.size / 8});
+    }
+    return bigger;
 }
 
 pub fn extractExtention(str: []const u8) fileError![]const u8 {
--- test_files.zig
+++ test_files.zig
@@ -4,6 +4,7 @@ const std = @import("std");
 const files = @import("files.zig");
 
 test "list files" {
-    try files.listFiles();
-    try files.FilesWeigth();
+    // try files.FilesWeigth("C:\\Users\\nboud\\downloads");
+    const biggest = try files.BiggestFile("C:\\Users\\nboud\\downloads");
+    print("biggest file is {s}", .{biggest.name});
 }
